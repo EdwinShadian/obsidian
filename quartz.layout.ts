04cd505d9c3df821ee4fc6abf5859a2926ed136f
EdwinShadian <captainbranan@gmail.com>
2024-12-23 17:25:21 +0200

    Quartz sync: Dec 23, 2024, 5:25 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -17,16 +17,17 @@ export const sharedPageComponents: SharedLayout = {
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
+    Component.Breadcrumbs(),
     Component.ArticleTitle(),
     Component.ContentMeta(),
     Component.TagList(),
   ],
   left: [
-    Component.DesktopOnly(Component.PageTitle()),
+    Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
-    Component.Explorer(),
     Component.Search(),
     Component.Darkmode(),
+    Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
     Component.Graph(),
@@ -43,7 +44,7 @@ export const defaultListPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    Component.Explorer(),
+    Component.DesktopOnly(Component.Explorer()),
   ],
   right: [],
 }
